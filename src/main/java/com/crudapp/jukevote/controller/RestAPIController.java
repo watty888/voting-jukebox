c866c618c512c21fc425587d341a936882093351
Bunyatov Elcin <a01449747@unet.univie.ac.at>
2021-05-06 02:18:13 +0200

    fixed upvote

--- src/main/java/com/crudapp/jukevote/controller/RestAPIController.java
+++ src/main/java/com/crudapp/jukevote/controller/RestAPIController.java
@@ -1,129 +1,128 @@
 package com.crudapp.jukevote.controller;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-
 import com.crudapp.jukevote.model.Message;
 import com.crudapp.jukevote.model.Song;
+import com.crudapp.jukevote.service.SongServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import com.crudapp.jukevote.service.SongServices;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/song")
 public class RestAPIController {
-	
-	@Autowired
-	SongServices songServices;
-	
-	@PostMapping("/create")
-	public ResponseEntity<Message> addNewSong(@RequestBody Song song) {
-		try {
-			Song returnedSong = songServices.saveSong(song);
-			
-			return new ResponseEntity<Message>(new Message("Upload Successfully!", 
-											Arrays.asList(returnedSong), ""), HttpStatus.OK);
-		}catch(Exception e) {
-			return new ResponseEntity<Message>(new Message("Fail to post a new Song!", 
-											null, e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);			
-		}
-	}
-	
-	@GetMapping("/retrieveinfos")
-	public ResponseEntity<Message> retrieveSongInfo() {
-		
-		try {
-			List<Song> songInfos = songServices.getSongInfos();
-			
-			return new ResponseEntity<Message>(new Message("Get Songs' Infos!",
-					songInfos, ""), HttpStatus.OK);
-		}catch(Exception e) {
-			return new ResponseEntity<Message>(new Message("Fail!",
-												null, e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-	}
-	
-	@GetMapping("/findone/{id}")
-	public ResponseEntity<Message> getSongById(@PathVariable long id) {
-		try {
-			Optional<Song> optSong = songServices.getSongById(id);
-			
-			if(optSong.isPresent()) {
-				return new ResponseEntity<Message>(new Message("Successfully! Retrieve a song by id = " + id,
-															Arrays.asList(optSong.get()), ""), HttpStatus.OK);
-			} else {
-				return new ResponseEntity<Message>(new Message("Failure -> NOT Found a song by id = " + id,
-						null, ""), HttpStatus.NOT_FOUND);
-			}
-		}catch(Exception e) {
-			return new ResponseEntity<Message>(new Message("Failure",
-					null, e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-	}
-	
-	@PutMapping("/updatebyid/{id}")
-	public ResponseEntity<Message> updateSongById(@RequestBody Song _song,
-																	@PathVariable long id) {
-		try {
-			if(songServices.checkExistedSong(id)) {
-				Song song = songServices.getSongById(id).get();
-				
-				//set new values for song
-				song.setTitle(_song.getTitle());
-				song.setArtist(_song.getArtist());
-				song.setVote(_song.getVote());
-	
-				// save the change to database
-				songServices.updateSong(song);
-				
-				return new ResponseEntity<Message>(new Message("Successfully! Updated a Song "
-																		+ "with id = " + id,
-																	Arrays.asList(song), ""), HttpStatus.OK);
-			}else {
-				return new ResponseEntity<Message>(new Message("Failer! Can NOT Found a Song "
-						+ "with id = " + id,
-					null, ""), HttpStatus.NOT_FOUND);
-			}
-		}catch(Exception e) {
-			return new ResponseEntity<Message>(new Message("Failure",
-					null, e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);			
-		}
-	}
 
-	@PutMapping("/upvote/{id}")
-	public void upvoteSong(@RequestBody Song _song, @PathVariable long id){
-		Song song = songServices.getSongById(id).get();
+    @Autowired
+    SongServices songServices;
 
-		song.setVote(_song.getVote()+1);
-		//song.incrementRating();
+    @PostMapping("/create")
+    public ResponseEntity<Message> addNewSong(@RequestBody Song song) {
+        try {
+            Song returnedSong = songServices.saveSong(song);
+
+            return new ResponseEntity<Message>(new Message("Upload Successfully!",
+                    Arrays.asList(returnedSong), ""), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<Message>(new Message("Fail to post a new Song!",
+                    null, e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/retrieveinfos")
+    public ResponseEntity<Message> retrieveSongInfo() {
+
+        try {
+            List<Song> songInfos = songServices.getSongInfos();
+
+            return new ResponseEntity<Message>(new Message("Get Songs' Infos!",
+                    songInfos, ""), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<Message>(new Message("Fail!",
+                    null, e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/findone/{id}")
+    public ResponseEntity<Message> getSongById(@PathVariable long id) {
+        try {
+            Optional<Song> optSong = songServices.getSongById(id);
+
+            if (optSong.isPresent()) {
+                return new ResponseEntity<Message>(new Message("Successfully! Retrieve a song by id = " + id,
+                        Arrays.asList(optSong.get()), ""), HttpStatus.OK);
+            } else {
+                return new ResponseEntity<Message>(new Message("Failure -> NOT Found a song by id = " + id,
+                        null, ""), HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<Message>(new Message("Failure",
+                    null, e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/updatebyid/{id}")
+    public ResponseEntity<Message> updateSongById(@RequestBody Song _song,
+                                                  @PathVariable long id) {
+        try {
+            if (songServices.checkExistedSong(id)) {
+                Song song = songServices.getSongById(id).get();
+
+                //set new values for song
+                song.setTitle(_song.getTitle());
+                song.setArtist(_song.getArtist());
+                song.setVote(_song.getVote());
+
+                // save the change to database
+                songServices.updateSong(song);
+
+                return new ResponseEntity<Message>(new Message("Successfully! Updated a Song "
+                        + "with id = " + id,
+                        Arrays.asList(song), ""), HttpStatus.OK);
+            } else {
+                return new ResponseEntity<Message>(new Message("Failer! Can NOT Found a Song "
+                        + "with id = " + id,
+                        null, ""), HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>(new Message("Failure",
+                    null, e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/upvote/{id}")
+    public ResponseEntity<Message> upvoteSong(@RequestBody Song _song, @PathVariable long id) {
+        Song song = songServices.getSongById(id).get();
+
+        song.setVote(_song.getVote() + 1);
+        //song.incrementRating();
+        songServices.upvoteSong(song);
+        return new ResponseEntity<>(new Message("Successfully! Upvoted a Song "
+                + "with id = " + id,
+                Arrays.asList(song), ""), HttpStatus.OK);
+    }
 
 
-		songServices.upvoteSong(song);
+    @DeleteMapping("/deletebyid/{id}")
+    public ResponseEntity<Message> deleteSongById(@PathVariable long id) {
+        try {
+            // checking the existed of a Song with id
+            if (songServices.checkExistedSong(id)) {
+                songServices.deleteSongById(id);
 
-	}
-
-
-	@DeleteMapping("/deletebyid/{id}")
-	public ResponseEntity<Message> deleteSongById(@PathVariable long id) {
-		try {
-			// checking the existed of a Song with id
-			if(songServices.checkExistedSong(id)) {
-				songServices.deleteSongById(id);
-				
-				return new ResponseEntity<Message> (new Message("Successfully! Delete a Song with id = " + id, 
-														null, ""), HttpStatus.OK);
-			}else {
-				return new ResponseEntity<Message>(new Message("Failer! Can NOT Found a Song "
-														+ "with id = " + id, null, ""), HttpStatus.NOT_FOUND);
-			}
-		}catch(Exception e) {
-			return new ResponseEntity<Message>(new Message("Failure",
-					null, e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-	}
+                return new ResponseEntity<Message>(new Message("Successfully! Delete a Song with id = " + id,
+                        null, ""), HttpStatus.OK);
+            } else {
+                return new ResponseEntity<Message>(new Message("Failer! Can NOT Found a Song "
+                        + "with id = " + id, null, ""), HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<Message>(new Message("Failure",
+                    null, e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
--- src/main/java/com/crudapp/jukevote/service/SongServices.java
+++ src/main/java/com/crudapp/jukevote/service/SongServices.java
@@ -1,48 +1,48 @@
 package com.crudapp.jukevote.service;
 
-import java.util.List;
-import java.util.Optional;
-
-import com.crudapp.jukevote.repository.SongRepository;
 import com.crudapp.jukevote.model.Song;
+import com.crudapp.jukevote.repository.SongRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 
 @Service
 public class SongServices {
-	
-	@Autowired
-	SongRepository repository;
-	
-	public Song saveSong(Song song) {
-		return repository.save(song);
-	}
-	
-	public List<Song> getSongInfos(){
-		return repository.findAll();
-	}
-	
-	public Optional<Song> getSongById(long id) {
-		return repository.findById(id);
-	}
-	
-	public boolean checkExistedSong(long id) {
-		if(repository.existsById((long) id)) {
-			return true;
-		}
-		return false;
-	}
-	
-	public Song updateSong(Song song) {
-		return repository.save(song);
-	}
 
-	public Song upvoteSong(Song song) {
-		return repository.save(song);
-	}
-	
-	public void deleteSongById(long id) {
-		repository.deleteById(id);
-	}
+    @Autowired
+    SongRepository repository;
+
+    public Song saveSong(Song song) {
+        return repository.save(song);
+    }
+
+    public List<Song> getSongInfos() {
+        return repository.findAll();
+    }
+
+    public Optional<Song> getSongById(long id) {
+        return repository.findById(id);
+    }
+
+    public boolean checkExistedSong(long id) {
+        if (repository.existsById((long) id)) {
+            return true;
+        }
+        return false;
+    }
+
+    public Song updateSong(Song song) {
+        return repository.save(song);
+    }
+
+    public Song upvoteSong(Song song) {
+        return repository.save(song);
+    }
+
+    public void deleteSongById(long id) {
+        repository.deleteById(id);
+    }
 }
